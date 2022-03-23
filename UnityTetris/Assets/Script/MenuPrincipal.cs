9b4ece5c01e901c1882a79ffd94678ee23a3b4ab
il01193 <113597434+GitMaily@users.noreply.github.com>
2022-03-23 16:26:33 +0000

    Menu principal du jeu : boutons démarrer, charger, et quitter. Continuer l'implémentation et le design de la sauvegarde

---
+++ UnityTetris/Assets/Script/MenuPrincipal.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuPrincipal : MonoBehaviour
+{
+
+    //public string partieCharger;
+    public string newPartie;
+    //Commencer une partie
+    public void StartGame()
+    {
+        Debug.Log("Une nouvelle partie a été lancée");
+        SceneManager.LoadScene("SampleScene");
+    }
+    
+    //Charger une partie
+    public void LoadSave()
+    {
+        //new SceneLoadInfo();
+        SceneManager.LoadScene("ListeSauvegarde");
+    }
+    
+    //Quitter l'application
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+    //Afficher les crédits
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
