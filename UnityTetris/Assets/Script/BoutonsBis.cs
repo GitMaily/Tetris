b8fd47a75307c9e65752b18b4266e89ec49564d3
il01193 <113597434+GitMaily@users.noreply.github.com>
2022-03-24 23:47:59 +0000

    Fusion de Bouton - BoutonBis et Mouvement - TetroCourrant (intégration fonctionnelle) Changement d'alignement/coordonnées et paramètres pour chaque Tetro des prefabs.

--- UnityTetris/Assets/Script/BoutonsBis.cs
+++ UnityTetris/Assets/Script/BoutonsBis.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 namespace Script
 {
@@ -14,7 +15,7 @@ namespace Script
             _tetroCourrant = tetroCourrant.GetComponent<TetroCourrant>();
         }
 
-        public void BottonCheck()
+        public void BoutonCheck()
         {
             if (Input.GetKeyDown(KeyCode.RightArrow))
             {
@@ -28,5 +29,54 @@ namespace Script
         }
         
         
+        public void BoutonEspace() // Descente
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            { 
+                _tetroCourrant.Descente();
+            }
+                
+        }
+        public void BoutonHaut() // Rotation gauche
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            { 
+               _tetroCourrant.RotationGauche();
+            }
+                
+        }
+            
+        public void BoutonBas() // Rotation droite
+        {
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                _tetroCourrant.RotationDroite();
+            }
+        }
+        
+        public void Tabulation() // Echange
+        {
+            if (Input.GetKeyDown(KeyCode.Tab ) || Input.GetKeyUp(KeyCode.Tab)) // Pour éviter d'appuyer sur tab 2 fois
+            // Problème : si on maintient la touche appuyée, aucun tetromino n'est généré
+            // Problème mineur, à corriger si on a le temps
+            {
+                
+                Debug.Log("Tabulation appuyée, chargée dans boutons.bis");
+                _tetroCourrant.GenererEchange();
+
+            }
+
+           
+        }
+
+        public void Echap() // Pause
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Debug.Log("Partie mise en pause : chargement du menu de pause");
+                SceneManager.LoadScene("MenuPause");
+            }
+        }
+
     }
 }
