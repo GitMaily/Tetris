43dd92fc802706099f619e931c700d25d5e907a1
im07571 <im07571@ca84efc1-3927-4b66-8990-7a0bd0e04871>
2022-03-30 16:15:11 +0000

    Implémentation de la sauvegarde.

--- UnityTetris/Assets/Script/Sauvegarder.cs
+++ UnityTetris/Assets/Script/Sauvegarder.cs
@@ -1,41 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
 
-public class Sauvegarder : MonoBehaviour
+
+namespace Script
 {
-    private int score;
+    [Serializable]
+    public class Sauvegarder
+    {
+        private int score;
 
-    public Sauvegarder()
-    {
-        score = 0;
-    }
+        public Sauvegarder()
+        {
+            score = 0;
+        }
     
-    public void Sauvegarde()
-    {
-        Sauvegarder fichier = new Sauvegarder();
-        IFormatter formatter = new BinaryFormatter();
-        Stream stream = new FileStream("GameSave.bin", FileMode.Create, FileAccess.Write, FileShare.None); /* problème */
-        Debug.Log("Création du fichier");
-        formatter.Serialize(stream, fichier); /* problème */
-        Debug.Log("Sérialisation");
-        stream.Close();
+        public void Sauvegarde()
+        {
+            Sauvegarder fichier = new Sauvegarder();
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream("GameSave.bin", FileMode.Create, FileAccess.Write, FileShare.None); /* problème */
+            Debug.Log("Création du fichier");
+            formatter.Serialize(stream, fichier); /* problème */
+            Debug.Log("Sérialisation");
+            stream.Close();
         
-        Debug.Log("Sauvegarde réussi");
-    }
+            Debug.Log("Sauvegarde réussi");
+        }
     
-    // Start is called before the first frame update
-    void Start()
-    {
+        // Start is called before the first frame update
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
     }
 }
